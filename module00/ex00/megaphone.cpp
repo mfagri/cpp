66fd4dc7b02f0ace90185307aed7df95188be721
Marouane Fagri <mfagri@e3r8p2.1337.ma>
2022-08-23 00:27:04 +0100

    module00_ex00

---
+++ module00/ex00/megaphone.cpp
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mfagri <mfagri@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/08/22 18:56:41 by mfagri            #+#    #+#             */
+/*   Updated: 2022/08/22 20:36:25 by mfagri           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <iostream>
+#include <string>
+
+int main(int ac,char **av)
+{
+    int i;
+    
+    int j;
+    
+    if(ac > 1)
+    {
+        i = 1;
+        while(av[i])
+        {
+            j = 0;
+            while(av[i][j])
+            {
+                if (islower(av[i][j]))
+                    av[i][j] = toupper(av[i][j]);
+                j++;
+            }
+            std::cout <<av[i];
+            std::cout <<" ";
+            i++;
+        }
+        std::cout <<"\n";
+        return (0);
+    }
+    else
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *\n";
+}
